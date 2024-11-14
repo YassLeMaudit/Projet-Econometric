f0f1d85dbcefea7b0229f22b8adc0fd83f63f3f7
Jaddevvv <jad@mendo.ai>
2024-11-14 13:45:08 +0100

    [ADD] - All transposed scenarios

--- all_scenarios.py
+++ all_scenarios.py
@@ -9,12 +9,14 @@ S2 = scenarios[scenarios['Scenario'] == 'S2'].transpose()
 S3 = scenarios[scenarios['Scenario'] == 'S3'].transpose()
 S4 = scenarios[scenarios['Scenario'] == 'S4'].transpose()
 
+
 S1.drop('Scenario', inplace=True)
 S2.drop('Scenario', inplace=True)
 S3.drop('Scenario', inplace=True)
 S4.drop('Scenario', inplace=True)
 
-S1.to_csv('data_output/S1.csv',index=False)
-S2.to_csv('data_output/S2.csv',index=False)
-S3.to_csv('data_output/S3.csv',index=False)
-S4.to_csv('data_output/S4.csv',index=False)
+
+S1.to_csv('data_output/S1.csv')
+S2.to_csv('data_output/S2.csv')
+S3.to_csv('data_output/S3.csv')
+S4.to_csv('data_output/S4.csv')
