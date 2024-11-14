0f7d8001a615a08f436923fb6d8d2d7eb3dff4b0
YassLeMaudit <145545350+YassLeMaudit@users.noreply.github.com>
2024-11-14 12:34:30 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import statsmodels.formula as smf
+import numpy as np
+
+df = pd.read_csv('Data/Data.csv')
+data_transpose = df.T
+data_transpose.to_csv('Data_transpose.csv', index= False)
